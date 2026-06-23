d04be8388f47c4ce2b78e443c0cc2e896b682f7c
Muhammad Saad <saad.cs.2026@gmail.com>
2026-06-23 21:34:37 +0500

    Update protect.js

--- public/js/protect.js
+++ public/js/protect.js
@@ -1,7 +1,7 @@
 // Simple Protection
 document.addEventListener('contextmenu', e => e.preventDefault());
 document.addEventListener('keydown', e => {
-  if (e.key === 'F12') e.preventDefault();
+  
   if (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'i')) e.preventDefault();
   if (e.ctrlKey && e.shiftKey && (e.key === 'J' || e.key === 'j')) e.preventDefault();
   if (e.ctrlKey && e.shiftKey && (e.key === 'C' || e.key === 'c')) e.preventDefault();
